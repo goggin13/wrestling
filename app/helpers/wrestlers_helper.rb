74f60d95f6bab9a165b140aae5eef738f14b5c16
goggin13 <goggin13@gmail.com>
2021-01-05 12:14:21 -0600

    Fix display view

--- app/helpers/wrestlers_helper.rb
+++ app/helpers/wrestlers_helper.rb
@@ -2,11 +2,10 @@ module WrestlersHelper
   DEFAULT_SIZE = "100x100"
 
   def avatar(wrestler, options={})
-    size = (options[:size] || DEFAULT_SIZE)
-    width, height = size.split("x")
-    options = {resize_to_limit: [width, height]}
+    options[:size] ||= DEFAULT_SIZE
+    width, height = options[:size].split("x")
     if wrestler.avatar.attached?
-      image_tag wrestler.avatar.variant(options)
+      image_tag wrestler.avatar.variant(resize_to_limit: [width, height]), options
     else
       image_tag "default_wrestler_avatar.png", options.merge(size: size)
     end
