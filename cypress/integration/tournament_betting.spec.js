a6da3bbdc24521a62e35601b90819f742590b5a5
goggin13 <goggin13@gmail.com>
2022-03-07 22:17:12 -0600

    fix test name

--- cypress/integration/tournament_betting.spec.js
+++ cypress/integration/tournament_betting.spec.js
@@ -1,7 +1,7 @@
 // var ROOT = 'http://localhost:3000/'
 var ROOT = 'http://wrastling-staging.herokuapp.com/'
 
-describe('Logging in', () => {
+describe('Tournament betting page', () => {
   it('shows the users balance', () => {
     cy.visit(ROOT + 'tournaments/1/bet?c=cGF0c3F1ZWdsaWFAZ21haWwuY29t')
     cy.contains('Welcome patsqueglia@gmail.com')
